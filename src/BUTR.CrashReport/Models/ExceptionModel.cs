696c2bcdef5f50c1a9b47fdbdec67146bec9ab2f
Vitalii Mikhailov <personal@aragas.org>
2023-10-17 14:05:27 +0300

    Fix

--- src/BUTR.CrashReport/Models/ExceptionModel.cs
+++ src/BUTR.CrashReport/Models/ExceptionModel.cs
@@ -4,8 +4,8 @@ namespace BUTR.CrashReport.Models;
 
 public record ExceptionModel
 {
-    public required string ModuleId { get; set; }
     public required string Type { get; set; }
+    public required string SourceModuleId { get; set; }
     public required string Message { get; set; }
     public required string CallStack { get; set; }
     public required ExceptionModel? InnerException { get; set; }
