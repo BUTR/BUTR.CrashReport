27e14e48599e6d96be8de6f2668f63f25f179518
Vitalii Mikhailov <personal@aragas.org>
2024-04-05 22:56:16 +0300

    Renamed Renderer to Renderer.ImGui

---
+++ src/BUTR.CrashReport.Renderer.ImGui/Extensions/RefLinqExtensions.cs
@@ -0,0 +1,12 @@
+﻿using BUTR.CrashReport.Renderer.ImGui.Utils;
+
+using HonkPerf.NET.RefLinq.Enumerators;
+
+using System.Collections.Generic;
+
+namespace BUTR.CrashReport.Renderer.ImGui.Extensions;
+
+internal static class RefLinqExtensions
+{
+    public static RefLinqEnumerable<T, IListEnumerator<T>> ToRefLinq<T>(this IList<T> c) => new(new(c));
+}
