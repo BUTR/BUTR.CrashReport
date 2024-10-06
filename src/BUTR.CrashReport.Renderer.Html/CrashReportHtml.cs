44b216f1975613e52929695cf323cad96e2f7e72
Vitalii Mikhailov <personal@aragas.org>
2024-10-06 12:01:32 +0300

    Corrected Unclassified detection in Html

--- src/BUTR.CrashReport.Renderer.Html/CrashReportHtml.cs
+++ src/BUTR.CrashReport.Renderer.Html/CrashReportHtml.cs
@@ -514,7 +514,8 @@ public static partial class CrashReportHtml
                 AssemblyModelType.Module => "module_assembly",
                 AssemblyModelType.Loader => "loader_assembly",
                 AssemblyModelType.LoaderPlugin => "loader_plugin_assembly",
-                _ => "unclas_assembly",
+                _ when assembly.Type == AssemblyModelType.Unclassified => "unclas_assembly",
+                _ => string.Empty
             }));
             var isDynamic = assembly.Type.HasFlag(AssemblyModelType.Dynamic);
             var hasPath = assembly.AnonymizedPath != "EMPTY" && !string.IsNullOrWhiteSpace(assembly.AnonymizedPath);
