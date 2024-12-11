7a40828ae9bf6728693b6d911dbd68108d50efd3
Vitalii Mikhailov <personal@aragas.org>
2024-12-12 00:41:31 +0200

    Added standalone and publish for it

--- src/BUTR.CrashReport.Renderer.Html/Extensions/EnumExtensions.cs
+++ src/BUTR.CrashReport.Renderer.Html/Extensions/EnumExtensions.cs
@@ -6,6 +6,10 @@ namespace BUTR.CrashReport.Renderer.Html.Extensions;
 
 internal static class EnumExtensions
 {
-    public static IEnumerable<TEnum> GetFlags<TEnum>(this TEnum input) where TEnum : Enum =>
+    public static IEnumerable<TEnum> GetFlags<TEnum>(this TEnum input) where TEnum : struct, Enum =>
+#if NET6_0_OR_GREATER
+        Enum.GetValues<TEnum>().Where(@enum => input.HasFlag(@enum));
+#else
         Enum.GetValues(input.GetType()).OfType<TEnum>().Where(@enum => input.HasFlag(@enum));
+#endif
 }
